a6d3c5bd0ebd84f43d873b04ed709bd74158deab
YONGHWA\ekore <yonghwa95@naver.com>
2024-02-19 08:33:30 +0900

    스프링 예외처리 복습

---
+++ src/main/java/hello/exeption/WebServerCustomizer.java
@@ -0,0 +1,18 @@
+package hello.exeption;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+
+//@Component
+public class WebServerCustomizer implements
+        WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/templates/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.NOT_FOUND, "/templates/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/templates/error-page/500");
+    }
+}
---
+++ src/main/java/hello/exeption/servlet/ErrorPageController.java
@@ -0,0 +1,24 @@
+package hello.exeption.servlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+
+    @GetMapping("/templates/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 404");
+        return "templates/error-page/404";
+    }
+
+    @GetMapping("/templates/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 500");
+        return "templates/error-page/500";
+    }
+}
